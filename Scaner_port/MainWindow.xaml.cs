1b990b8532bcf55e9daaac35d2a644609c8552dd
NikTot <Niktotem@gmail.com>
2016-12-16 13:50:06 +0700

    AddRepository

---
+++ Scaner_port/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using MahApps.Metro.Controls;
+
+
+
+namespace Scaner_port
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        private GridViewColumnHeader listViewSortCol = null;
+        private SortAdorner listViewSortAdorner = null;
+        public string[] ipTileService = new string[] { "91.236.50.81", "91.236.50.36", "91.236.50.33", "10.54.18.212", "10.54.18.210" };
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<PortInfo> pi = MainWindow.GetOpenPort();
+            listView.ItemsSource = pi;
+        }
+
+        private static List<PortInfo> GetOpenPort()
+        {
+            IPGlobalProperties properties = IPGlobalProperties.GetIPGlobalProperties();
+            IPEndPoint[] tcEndPoints = properties.GetActiveTcpListeners();
+            TcpConnectionInformation[] tcpConnections = properties.GetActiveTcpConnections();
+
+            return tcpConnections.Select(p =>
+            {
+                return  new PortInfo(
+                    i: p.LocalEndPoint.Port,
+                    local: string.Format("{0}", p.LocalEndPoint.Address),
+                    remote: string.Format("{0}", p.RemoteEndPoint.Address),
+                    state: p.State.ToString()
+                    );
+            }).ToList();
+        }
+
+        private void GridViewColumnHeader_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            GridViewColumnHeader column = (sender as GridViewColumnHeader);
+            string sortBy = column.Tag.ToString();
+            if (listViewSortCol != null)
+            {
+                AdornerLayer.GetAdornerLayer(listViewSortCol).Remove(listViewSortAdorner);
+                listView.Items.SortDescriptions.Clear();
+            }
+
+            ListSortDirection newDir = ListSortDirection.Ascending;
+            if (listViewSortCol == column && listViewSortAdorner.Direction == newDir)
+                newDir = ListSortDirection.Descending;
+
+            listViewSortCol = column;
+            listViewSortAdorner = new SortAdorner(listViewSortCol, newDir);
+            AdornerLayer.GetAdornerLayer(listViewSortCol).Add(listViewSortAdorner);
+            listView.Items.SortDescriptions.Add(new SortDescription(sortBy, newDir));
+
+        }
+
+        private void btnUpdate_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            List<PortInfo> pi = MainWindow.GetOpenPort();
+            listView.ItemsSource = pi;
+        }
+    }
+}
---
+++ Scaner_port/PortInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace Scaner_port
+{
+    class PortInfo
+    {
+        public int PortNumber { get; set; }
+        public string Local { get; set; }
+        public string Remote { get; set; }
+        public string State { get; set; }
+
+        public PortInfo (int i, string local, string remote, string state)
+        {
+            PortNumber = i;
+            Local = local;
+            Remote = remote;
+            State = state;
+        }
+    }
+}
